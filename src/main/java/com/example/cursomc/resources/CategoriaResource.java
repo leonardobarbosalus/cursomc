6d81cdbc910fcb68251b88d908c7f1998d82e340
leonardo barbosa <leolbarbosa09@gmail.com>
2021-08-05 21:04:04 -0300

    DELETANDO UMA CATEGORIA

--- src/main/java/com/example/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,15 @@
 package com.example.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.cursomc.domain.Categoria;
 import com.example.cursomc.services.CategoriaService;
@@ -26,6 +27,31 @@ public class CategoriaResource {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> Insert (@RequestBody Categoria cat){
+		
+		cat = service.Insert(cat);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cat.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
+	}
+	
+	@RequestMapping(value = "/{id}" , method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria cat , @PathVariable Integer id){
+		
+		cat.setId(id);
+		cat = service.update(cat);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}" , method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/example/cursomc/services/CategoriaService.java
+++ src/main/java/com/example/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.example.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.example.cursomc.domain.Categoria;
 import com.example.cursomc.repositories.CategoriaRepository;
+import com.example.cursomc.services.exception.DataIntegrityException;
 import com.example.cursomc.services.exception.ObjectNotFountException;
 
 
@@ -22,4 +24,23 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
+	public Categoria Insert (Categoria cat) {
+		cat.setId(null);
+		return repo.save(cat);
+	}
+	
+	public Categoria update (Categoria cat) {
+		
+		find(cat.getId());
+		return repo.save(cat);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}catch(DataIntegrityViolationException e ) {
+			throw new DataIntegrityException("Não é possivel excluir!");
+		}
+	}
 }
